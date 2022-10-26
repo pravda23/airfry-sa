c958051b5fe74873c78cdb4ed91397a6603a4696
pravda23 <pravda23@gmail.com>
2022-10-26 11:26:47 +0200

    initial

---
+++ app.js
@@ -0,0 +1,52 @@
+// loads hamburger menu overlay on click
+
+window.onload = () => {
+  // Add a listener for any clicks on the menu
+  document
+    .getElementById("menu-overlay")
+    .addEventListener("click", function () {
+      hamburgerState = true;
+      console.log(hamburgerState);
+      document.getElementById("toggler").checked = false;
+    });
+};
+
+// image slider gallery
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  console.log(slideIndex);
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  console.log("minus");
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("g1-slides");
+  console.log(slides[0]);
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
